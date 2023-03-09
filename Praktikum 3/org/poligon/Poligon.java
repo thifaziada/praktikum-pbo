03ebba68b6ba7ac5ef173dfc86d5111436790463
thifaziada <100354815+thifaziada@users.noreply.github.com>
2023-03-09 20:54:09 +0700

    Add files via upload

---
+++ Praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,16 @@
+/**
+
+	*File : Poligon.java 08/03/2023
+	*Penulis : Thifa Ziada Taqiyya
+	*Deskripsi : representasi dasar dari objek poligon (segi banyak)
+*/
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+
+}
