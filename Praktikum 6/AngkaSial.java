faac83fc3aaa96f29c0b8c3997ad47ae4cf74e9f
thifaziada <100354815+thifaziada@users.noreply.github.com>
2023-04-05 11:21:06 +0700

    Update AngkaSial.java

--- Praktikum 6/AngkaSial.java
+++ Praktikum 6/AngkaSial.java
@@ -28,4 +28,14 @@ public class AngkaSial{
 }
 //PERTANYAAN: 
 //Ketika eksepsi terjadi, apakah baris 12 pada AngkaSial.java di atas dieksekusi? 
-//Apakah baris 21 pada AngkaSial.java di atas dieksekusi? 
+//Jawab : tidak, karena Ketika sebuah eksepsi terjadi, maka program akan mencari 
+//	blok penanganan eksepsi yang cocok sedangkan pada baris 12 bukanlah kode 
+//	untuk penanganan eksepsi yang cocok untuk menangani eksepsi yang terjadi, 
+//	maka baris 12 pada program tersebut tidak dieksekusi
+
+//Apakah baris 21 pada AngkaSial.java di atas dieksekusi?
+//Jawab : ya, karena Ketika sebuah eksepsi (exception) terjadi dalam program Java, 
+//	maka alur eksekusi program normal akan terhenti dan program akan mencari 
+//	blok penanganan eksepsi (exception handling block) yang cocok untuk menangani 
+//	eksepsi tersebut, yaitu ditemukan pada baris 21, maka kode di dalam baris 21 
+//	akan dieksekusi untuk menangani eksepsi tersebut.
